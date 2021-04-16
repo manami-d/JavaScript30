3c9e8bedac3ae488fa6aab7df8b881b913fae38b
manami-d <manami.davidson@hotmail.com>
2021-04-16 12:49:09 +1000

    Complete day29 exercise

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,53 @@
+let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
+
+const timer = (seconds) => {
+  //clear any existing timer
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+};
+
+const displayTimeLeft = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSecs = seconds % 60;
+  const display = `${minutes}:${remainderSecs < 10 ? "0" : ""}${remainderSecs}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+};
+
+const displayEndTime = (timestamp) => {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+};
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach((button) => button.addEventListener("click", startTimer));
+document.customForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const mins = e.target.minutes.value;
+  timer(mins * 60);
+  e.target.reset();
+});
